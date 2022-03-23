211a15063809b1c9436e1dbaae385301cd1decd2
Jeremy Mann <jmann277@gmail.com>
2022-03-22 20:47:46 -0400

    add measurement stuff

--- tests/test_measurement.py
+++ tests/test_measurement.py
@@ -1,12 +1,50 @@
+from datetime import datetime
+
 import pytest
 
-from jer_cdm.measurement import MeasurementSchema, get_mock_measurement_row
+from jer_cdm.measurement import (MeasurementSchema, OMOPMeasurementSchema,
+                                 add_measurement_row, get_measurement_data,
+                                 get_measurement_row,
+                                 get_mock_omop_measurement_row)
+from jer_cdm.time import get_current_time
 
 
-@pytest.fixture
+@ pytest.fixture
 def measurement_df():
-    return get_mock_measurement_row()
+    return get_measurement_data()
 
 
-def test_mock_fits_schema(measurement_df):
-    MeasurementSchema.validate(get_mock_measurement_row())
+@ pytest.fixture
+def mock_measurement_df():
+    return get_mock_omop_measurement_row()
+
+
+@ pytest.fixture
+def measurement_row(measurement_concept, ontology):
+    measurement_row = get_measurement_row(
+        measurement_concept,
+        get_current_time(),
+        115.0,
+        ontology.get_unit(measurement_concept),
+    )
+    return measurement_row
+
+
+def test_personal_data_fits_schema(measurement_df):
+    MeasurementSchema.validate(measurement_df)
+
+
+def test_mock_fits_schema(mock_measurement_df):
+    OMOPMeasurementSchema.validate(get_mock_omop_measurement_row())
+
+
+def test_make_measurement_row(measurement_row):
+    MeasurementSchema.validate(measurement_row)
+
+
+def test_add_measurement_row(measurement_row, measurement_df):
+    new_measurement_df = add_measurement_row(
+        measurement_row, measurement_df
+    )
+    MeasurementSchema.validate(new_measurement_df)
+    assert (new_measurement_df.iloc[-1, :] == measurement_row.iloc[0, :]).all()
